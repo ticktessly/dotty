2075c03525a7eb09652cae5a2738928dc1639dc6
ticktessly <eepyashell@gmail.com>
2026-08-09 06:35:59 +0000

    github dark dimmed lowky cool as shii

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,12 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false,
 		priority = 1000,
-		opts = {
-			default_integrations = false,
-			integrations = {
-				treesitter = true,
-				native_lsp = { enabled = true },
-				mason = false,
-				mini = true,
-				flash = true,
-			},
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
+		config = function()
+			require("github-theme").setup({})
+			vim.cmd.colorscheme("github_dark_dimmed")
 		end,
 	},
 }
